2ddd1a7183121776c68080140368022f77276961
Дарья Зуева <gr432_zudev@lab-203-8.tomtit-tomsk.ru>
2026-03-02 12:24:03 +0700

    practical work 4 done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 let app = new Vue({
-   el: '#app',
-   data: {
-       product: "Socks",
-       image: "./assets/vmSocks-green-onWhite.jpg",
-       altText: "A pair of socks",
-       inStock: true,
-       inventory: 9,
-       onSale: false
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green'
+        },
+        {
+            variantId: 2235,
+            variantColor: 'blue'
+        }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
    }
+   
 })
 
