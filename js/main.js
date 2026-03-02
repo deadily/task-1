0bf1a3c7e8531ff3b2747f24f66ddfa20f682ada
dara <zuevadara04@gmail.com>
2026-03-02 23:02:35 +0700

    full code from 5th practice work

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ let app = new Vue({
    el: '#app',
    data: {
         product: "Socks",
+        description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
+        inventory: 9,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -18,7 +22,7 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
             ],
-
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
     },
     methods: {
